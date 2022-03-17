6816e37e8f2d5b7703cb4bac7799f3c145523c7c
tomeichlersmith <eichl008@umn.edu>
2022-03-17 13:46:09 -0500

    mild cleanup of survey script

--- hardware/survey.sh
+++ hardware/survey.sh
@@ -4,7 +4,16 @@
 
 # loop over hosts executing the passed arguments on each host in sequence
 __loop_over_hosts__() {
-  for h in cdms-04-{1..4} cdms{1,3} gopher-{01..12}-{1..4} scorpion{1..48} zebra0{1..4} spa-cdms2 twins-a{07..24} twins-b{01..23} wn01-{1,2,4} wn02-2; do
+  for h in cdms-04-{1..4} \
+           cdms{1,3} \
+           gopher-{01..12}-{1..4} \
+           scorpion{1..48} \
+           zebra0{1..4} \
+           spa-cdms2 \
+           twins-a{07..24} \
+           twins-b{01..23} \
+           wn01-{1,2,4} wn02-2
+  do
     printf "%12s\t" $h
     if ! ssh -q -o "StrictHostKeyChecking no" -o "ConnectTimeout 5" $h $@; then
       printf "%5s\t%5s\t%5s\t%4s\t%7s\t%4s\t%s\n" NA NA NA NA NA NA NA
@@ -12,7 +21,7 @@ __loop_over_hosts__() {
   done
 }
 
-#Host nproc RAM scratch CVMFS HDFS
+#Host nproc RAM CVMFS HDFS scratch sing OS
 printf "%12s\t%5s\t%5s\t%5s\t%4s\t%7s\t%4s\t%s\n" Host nproc RAM CVMFS HDFS scratch sing OS
 __loop_over_hosts__ \
   printf \"\%5d\\t\" \$\(nproc\) \;\
