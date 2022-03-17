4d774f39424de33dfe5abbad4812b338bad98577
tomeichlersmith <eichl008@umn.edu>
2022-03-17 10:03:53 -0500

    add connect timeout to avoid hanging on nodes that are down

--- survey.sh
+++ survey.sh
@@ -4,9 +4,9 @@
 
 # loop over hosts executing the passed arguments on each host in sequence
 __loop_over_hosts__() {
-  for h in cdms-04-{1..4} cdms{1,3} gopher-{01..12}-{1..4} scorpion{1..6} scorpion{8..48} zebra0{1..4} spa-cdms2 twins-a{07..24} twins-b{01..23} wn01-{1,2,4} wn02-2; do
+  for h in cdms-04-{1..4} cdms{1,3} gopher-{01..12}-{1..4} scorpion{1..48} zebra0{1..4} spa-cdms2 twins-a{07..24} twins-b{01..23} wn01-{1,2,4} wn02-2; do
     printf "%12s\t" $h
-    if ! ssh -q -o "StrictHostKeyChecking no" $h $@; then
+    if ! ssh -q -o "StrictHostKeyChecking no" -o "ConnectTimeout 5" $h $@; then
       echo "can't connect"
     fi
   done
