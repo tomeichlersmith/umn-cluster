9ec5bb48897ebdc490d2d20fd40e73be91a9e3cf
tomeichlersmith <eichl008@umn.edu>
2022-03-17 13:10:51 -0500

    use row of NA for not connected nodes

--- survey.sh
+++ survey.sh
@@ -7,7 +7,7 @@ __loop_over_hosts__() {
   for h in cdms-04-{1..4} cdms{1,3} gopher-{01..12}-{1..4} scorpion{1..48} zebra0{1..4} spa-cdms2 twins-a{07..24} twins-b{01..23} wn01-{1,2,4} wn02-2; do
     printf "%12s\t" $h
     if ! ssh -q -o "StrictHostKeyChecking no" -o "ConnectTimeout 5" $h $@; then
-      echo "can't connect"
+      printf "%5s\t%5s\t%5s\t%4s\t%7s\t%4s\t%s\n" NA NA NA NA NA NA NA
     fi
   done
 }
